23e7487d88b3b2e7bd4251661152de2dc7a7ea89
Sam <sam.tdc08@gmail.com>
2013-10-11 19:20:19 +1030

    Version 2
    Version 2 of PiBell
    Added Config file (PiBell.config)

--- PiBell.py
+++ PiBell.py
@@ -4,30 +4,37 @@ from email.mime.text import MIMEText
 
 import datetime
 now = datetime.datetime.now()
+config = open('PiBell.config').readlines()
 
-#Put the message that you want to be put into the terminal when the code is started (when the bell is rung) between the " and " below
 print "[PiBell] Ding, Dong!!! Doorbell rung."
 
-USERNAME = "<Put the username (email address) of YOUR *Gmail* (HAS to be Gmail) account here>"
-PASSWORD = "<Put the password of YOUR Gmail account here>"
-To1 = "<Put the recipient address here>"
+"""
+*!!!*!!!*!!!* 
+NOTE!!!
+ To edit the details (Username, Password and Recipient),
+ edit the 'PiBell.config' file
 
-#Put the message between the ' and ' on the next line
+ To edit the file, open LXTerminal and type 'leafpad PiBell.config'
+*!!!*!!!*!!!*
+"""
+
+
+USERNAME = config[1].strip()
+PASSWORD = config[4].strip()
+RECIPIENT = config[7].strip()
+
+
 msg = MIMEText('The doorbell has been rung!' + '\nRung at ' + now.strftime("%H:%M") + ' on ' + now.strftime("%d/%m/%Y") + '\n  ' + '\n--' + '\nSent via PiBell - Made by Sam Smith')
-
-#Put what you want the email's subject line to be between the ' and ' on the next line
 msg['Subject'] = '[PiBell] Ding, Dong!'
-
 msg['From'] = USERNAME
-msg['To'] = To1
+msg['To'] = RECIPIENT
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.ehlo_or_helo_if_needed()
 server.starttls()
 server.ehlo_or_helo_if_needed()
 server.login(USERNAME,PASSWORD)
-server.sendmail(USERNAME, To1, msg.as_string())
+server.sendmail(USERNAME, RECIPIENT, msg.as_string())
 server.quit()
 
-#Put the message that you want to be put into the terminal AFTER the email is sent between the " and " below (Currently says the recipient and the time/date)
-print "[PiBell] Email notification sent to " + To1 + " at " + now.strftime("%d/%m/%Y %H:%M")
+print "[PiBell] Email notification sent to " + RECIPIENT + " at " + now.strftime("%d/%m/%Y %H:%M")
